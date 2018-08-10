6072442eaec5dc0ac334efdba0b6a50fb8f9ec49
Andrew Wu <ajwu11@gmail.com>
2018-08-09 22:14:50 -0700

    Allow users to request an endpoint then render response

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
+require 'open-uri'
+
 class PagesController < ApplicationController
+  SWAPI_BASE_URL = "https://swapi.co/api"
+
   def home
+    if endpoint = params[:endpoint]
+      endpoint = "/#{endpoint}" unless endpoint.start_with?('/')
+
+      begin
+        @response = JSON.load(open("#{SWAPI_BASE_URL}#{endpoint}"))
+      rescue => e
+        @response = { error: e }
+      end
+    end
+
+    respond_to do |format|
+      format.js
+      format.html
+    end
   end
 end
