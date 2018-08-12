953c5a9137b8b877dc32ea0b4a9ab01b63461bc6
Andrew Wu <ajwu11@gmail.com>
2018-08-12 12:20:13 -0700

    Refactor Person model and tests to use shared code

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,10 @@
 class Person < ApplicationRecord
+  include Validatable
+
   VALID_BIRTH_YEAR_REGEX = /\A(unknown|.*BBY|.*ABY)\z/
+
+  NUMBER_ATTRIBUTES = [:mass, :height]
+  URL_ARRAY_ATTRIBUTES = [:films, :species, :starships, :vehicles]
 
   validates :name, presence: true
   validates :birth_year, presence: true,
@@ -36,45 +41,5 @@ class Person < ApplicationRecord
       edited: edited,
       url: url
     }
-  end
-
-  private
-
-  def check_numbers
-    [:mass, :height].each do |attribute|
-      if !is_number?(send(attribute))
-        errors.add(attribute, "must be a number")
-      end
-    end
-  end
-
-  def check_url_array
-    [:films, :species, :starships, :vehicles].each do |attribute|
-      if send(attribute).any?
-        checked_values = send(attribute).map do |val|
-          SWAPI_URL_REGEX.match(val)
-        end
-
-        if checked_values.include?(nil)
-          errors.add(attribute, "contains an invalid value")
-        end
-      end
-    end
-  end
-
-  def check_iso_8601
-    [:created, :edited].each do |attribute|
-      if !is_iso_8601?(send(attribute))
-        errors.add(attribute, 'must be in ISO 8601 format')
-      end
-    end
-  end
-
-  def is_iso_8601?(value)
-    Time.iso8601(value) rescue nil
-  end
-
-  def is_number?(value)
-    Float(value) rescue nil
   end
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,14 +1,19 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
+  include SwapiTest
+
+  @url_array_attributes = [:films, :species, :starships, :vehicles]
+  @url_attributes = [:homeworld, :url]
+  @number_attributes = [:height, :mass]
+
   setup do
-    @person = people(:luke)
+    @person = @object = people(:luke)
   end
 
-  test "should not save person without name" do
-    @person.name = nil
-    assert_not @person.save
-  end
+  self.test_number_attributes
+  self.test_url_attributes
+  self.test_url_array_attributes
 
   test "should not save person with invalid birth year values" do
     invalid_values = ['foobar', 2018, nil]
@@ -42,24 +47,6 @@ class PersonTest < ActiveSupport::TestCase
     end
   end
 
-  [:height, :mass].each do |attribute|
-    test "should not save person with invalid #{attribute} values" do
-      invalid_values = ['one', nil]
-      invalid_values.each do |val|
-        @person[attribute] = nil
-        assert_not @person.save
-      end
-    end
-
-    test "should save person with valid #{attribute} values" do
-      valid_values = %w(177 23.0 0)
-      valid_values.each do |val|
-        @person[attribute] = val
-        assert @person.save
-      end
-    end
-  end
-
   [:eye_color, :gender, :hair_color].each do |attribute|
     test "person's #{attribute} defaults to n/a" do
       person = Person.new
@@ -72,63 +59,4 @@ class PersonTest < ActiveSupport::TestCase
     assert_not @person.save
   end
 
-  [:homeworld, :url].each do |attribute|
-    test "should not save person with invalid #{attribute} values" do
-      invalid_values = %w(earth 123 1.0)
-      invalid_values.each do |val|
-        @person[attribute] = val
-        assert_not @person.save
-      end
-    end
-
-    test "should person with valid #{attribute} value" do
-      @person[attribute] = "https://swapi.co/api/planets/41/"
-      assert @person.save
-    end
-  end
-
-  [:films, :species, :starships, :vehicles].each do |attribute|
-    test "person's #{attribute} defaults to an empty array" do
-      person = Person.new
-      assert_empty person.send(attribute)
-    end
-
-    test "should not save person with invalid #{attribute}" do
-      invalid_values = %w(swapi.co abc 123 https://swapi.co/api/planets/28/)
-      @person[attribute] = invalid_values
-      assert_not @person.save
-    end
-
-    test "should save person with valid #{attribute}" do
-      valid_values = %w(https://swapi.co/api/planets/28/
-                        https://swapi.co/api/people/1/)
-      @person[attribute] = valid_values
-      assert @person.save
-    end
-  end
-
-  [:created, :edited].each do |attribute|
-    test "should not save person with invalid #{attribute} values" do
-      invalid_values = ['2015-04-17T06:57:38.061346Z.bad', '12:00PM', nil]
-      invalid_values.each do |val|
-        @person[attribute] = val
-        assert_not @person.save
-      end
-    end
-
-    test "should save person with valid #{attribute} values" do
-      @person[attribute] = "2015-04-17T06:57:38.061346Z"
-      assert @person.save
-    end
-  end
-
-  test "should not save person if swapi_id is taken" do
-    duped_person = @person.dup
-    assert_not duped_person.save
-  end
-
-  test "should not save person without swapi_id" do
-    @person.swapi_id = nil
-    assert_not @person.save
-  end
 end
